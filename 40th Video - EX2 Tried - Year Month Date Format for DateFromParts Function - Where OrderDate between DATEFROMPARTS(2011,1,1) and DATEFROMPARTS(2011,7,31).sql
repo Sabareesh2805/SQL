851c75df77ebb0e5ef3910fa01ee6f4932bab0e2
Sabareesh Jagannathan Rengarajan <85886878+Sabareesh2805@users.noreply.github.com>
2022-02-28 21:11:55 +0530

    16th February 2022 Files
    16th February 2022 Files

---
+++ 40th Video - EX2 Tried - Year Month Date Format for DateFromParts Function - Where OrderDate between DATEFROMPARTS(2011,1,1) and DATEFROMPARTS(2011,7,31).sql
@@ -0,0 +1,13 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+       [PurchaseOrderID]
+      ,[OrderDate]
+      ,[TotalDue]
+     
+      
+      
+  FROM [AdventureWorks2019].[Purchasing].[PurchaseOrderHeader]
+
+  Where OrderDate between DATEFROMPARTS(2011,1,1) and DATEFROMPARTS(2011,7,31)
+  And TotalDue > 10000
+  
